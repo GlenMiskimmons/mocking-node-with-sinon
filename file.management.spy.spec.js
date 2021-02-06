07da4ca8fdb9b13f150b1800ba3e19e96c31a6fb
GlenMiskimmons <42587792+GlenMiskimmons@users.noreply.github.com>
2021-02-06 15:14:27 +0000

    Module 02 Using a Spy
    Using a Spy

--- file.management.spy.spec.js
+++ file.management.spy.spec.js
@@ -5,7 +5,32 @@ const proxyquire = require("proxyquire");
 const fileManagement = require("./file.management");
 
 describe("File Management", () => {
-  it("true should be true", () => {
-    expect(true).to.be.true;
+  describe("When creating a new File", () => {
+    it("Should call writeFileSync", () => {
+      const writeSpy = spy(fs, "writeFileSync");
+      const fileManagement = proxyquire('./file.management', { fs });
+      fileManagement.createFile("test.txt");
+
+      expect(writeSpy.calledWith("./data/test.txt", "")).to.be.true;
+    });
+
+    it("Should not create a new file if no file is specified", () => {
+      const writeSpy = spy(fs, "writeFileSync");
+      const fileManagement = proxyquire('./file.management', { fs });
+
+      try {
+        fileManagement.createFile();
+      } catch (error) {
+        expect(writeSpy.notCalled).to.be.true;
+      }
+      
+    });
+
+    it.skip("Should call writeFileSync - injected", () => {
+      const writeSpy = spy(fs, "writeFileSync");
+      fileManagement.createFileInjected("test.txt", fs);
+
+      expect(writeSpy.calledWith("./data/test.txt", "")).to.be.true;
+    });
   });
 });
